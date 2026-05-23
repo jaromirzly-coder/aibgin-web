2eae0119b2cfd2529f4ebd657ad67ae70e5dd540
Jaromir Zly <jaromirzly@gmail.com>
2026-05-23 07:24:27 +0000

    feat: add Google Analytics G-F964Z5G173

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import CookieConsent from "./CookieConsent";
+
+const GA_ID = "G-F964Z5G173";
 
 const inter = Inter({ subsets: ["latin", "latin-ext"] });
 
@@ -28,6 +31,18 @@ export default function RootLayout({
   return (
     <html lang="cs" className={inter.className}>
       <body>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="ga-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}', { page_path: window.location.pathname });
+          `}
+        </Script>
         {children}
         <CookieConsent />
       </body>
